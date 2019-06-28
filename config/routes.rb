052d074d1da263ee79828af7916d819119780425
MSuzu190331 <m.suzuki.0u0@gmail.com>
2019-06-28 17:23:41 +0900

    create controller and index.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get   'tweets'      =>  'tweets#index'     #ツイート一覧画面
-  get   'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
-  post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
+  root 'tweets#index'     #ツイート一覧画面
+  resources :tweets     #ツイート投稿機能
 end
